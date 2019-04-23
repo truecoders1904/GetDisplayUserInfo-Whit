28b63bba396e2832e9f0de14fa2a300ed0f9a893
Whit <whit@stroups.net>
2019-04-22 23:50:13 -0500

    added clors for favenum

--- Program.cs
+++ Program.cs
@@ -8,12 +8,33 @@ namespace GetUserInfo
         {
             Person person1 = new Person();
             person1.firstName = person1.askQuestion("What is your cool, first name?");
+            Console.Clear();
             person1.lastName = person1.askQuestion("What is your last name?");
+            Console.Clear();
             person1.favoriteAnimal = person1.askQuestion("What is your favorite animal?");
-            person1.age = person1.askNumberQuestion("What is your age?");
+            Console.Clear();
+            person1.favenum = person1.askfaveNumberQuestion("What is your favorite number?");
+            Console.Clear();
             person1.WisdomScore = person1.askNumberQuestion("From 1 to 100, What is your wisdom score?");
+            Console.Clear();
 
-            Console.WriteLine($"Your first name is {person1.firstName} and your last name is {person1.lastName}. /n Your favorite animal is {person1.favoriteAnimal} and you are {person1.age} years old. /n You have a wisdom score of {person1.WisdomScore}.That's nice.");
+            Console.WriteLine($"Hello, {person1.firstName} {person1.lastName}!");
+            Console.WriteLine($"Your favorite animal is {person1.favoriteAnimal} and your favorite number is:");
+            if (person1.favenum < 5)
+            {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(person1.favenum);
+                    Console.ResetColor();
+
+            }
+                else if (person1.favenum > 10)
+            {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(person1.favenum);
+                    Console.ResetColor();
+            }
+
+            Console.WriteLine($"You have a wisdom score of {person1.WisdomScore}.That's nice.");
         }
     }
 
@@ -22,7 +43,8 @@ namespace GetUserInfo
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string favoriteAnimal { get; set; } 
-        public int age { get; set; }
+        public int favenum { get; set; }
+
         public int WisdomScore { get; set; }
 
         public string askQuestion(string question)
@@ -37,10 +59,16 @@ namespace GetUserInfo
             string answer = askQuestion(question);
             return int.Parse(answer);
 
+        }
 
-
+        public int askfaveNumberQuestion(string question)
+        {
+            string answer = askQuestion(question);
+            return int.Parse(answer);
 
         }
-    
+
+
+
     }
 }
